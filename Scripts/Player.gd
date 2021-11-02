398a7278c3f0f4251171b0f96d9821a8f44e6cea
TimJ5 <93225483+TimJ5@users.noreply.github.com>
2021-11-01 22:19:20 -0500

    Added the Main and Player scenes

---
+++ Scripts/Player.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const UP_DIRECTION = Vector2.UP
+const GRAVITY = 1000
+var air_jumps = 3
+var jump_strength = 500
+var velocity = Vector2.ZERO
+var speed = 400
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
